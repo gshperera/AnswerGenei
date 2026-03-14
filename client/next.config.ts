964e13acea281d46a7040762efe9ad54d2947c86
Shanika Perera <86468513+gshperera@users.noreply.github.com>
2026-03-14 17:06:55 +0530

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',  // This enables standalone output for Docker
+  output: 'standalone', 
+   // Log env vars during build
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },// This enables standalone output for Docker
 };
+
+// THIS WILL SHOW DURING RENDER BUILD
+console.log('🔧 NEXT_PUBLIC_API_URL during build:', process.env.NEXT_PUBLIC_API_URL);
+console.log('🔧 All env vars:', Object.keys(process.env).filter(key => key.includes('NEXT')));
 
 export default nextConfig;
